891e0bc0c811ae4b804fe6a1ea20daa772ee57b8
John Caldwell <john.caldwell@caldwelldevelopment.com>
2024-04-06 06:38:33 -0400

    Created two variables - side is for the length of the line to create the triangle and color is to change the color of the line.

--- turtle_party.py
+++ turtle_party.py
@@ -2,10 +2,13 @@ import turtle
 
 # created the triangle with a for loop with function and the triangle is still upside down.
 
+side = 100
+color = 'blue'
+
 def shape ():
-    turtle.color('red')
+    turtle.color(color)
     for line in range(3):
-        turtle.forward(100)
+        turtle.forward(side)
         turtle.right(120)
 
 #function to draw the triangle
