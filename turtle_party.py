bf202f195469abe78651deb2ca885f47eec819ab
John Caldwell <john.caldwell@caldwelldevelopment.com>
2024-04-06 06:12:05 -0400

    First version of the triangle project. The programming creates a triangle upside down and change the color of the line to red.

---
+++ turtle_party.py
@@ -0,0 +1,13 @@
+import turtle
+
+# This creates a triangle upside down and change the color of the triangle
+
+turtle.color('red')
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.mainloop()
