e386672b3d4eb29daae8f0bbe235f3766254d176
John Caldwell <john.caldwell@caldwelldevelopment.com>
2024-04-06 07:08:31 -0400

    Added if statement to the shape function to determ if the shape is being draw with left or right turns.

--- turtle_party.py
+++ turtle_party.py
@@ -6,22 +6,28 @@ side = 100
 color = 'blue'
 number_of_sides = 3
 angle_for_side = 120
+upsidedown = "right"
 
 
-def shape(side=50, color='black', number_of_sides=3, angle_for_side=360):
+def shapes(side=50, color='black', number_of_sides=3, upsidedown='right', angle_for_side=360):
     turtle.color(color)
     for line in range(number_of_sides):
         turtle.forward(side)
-        turtle.right(angle_for_side / number_of_sides)
+        if upsidedown == "right":
+            turtle.right(angle_for_side / number_of_sides)
+        elif upsidedown == "left":
+            turtle.left(angle_for_side / number_of_sides)
 
 
-def move():
+def move(len):
     turtle.penup()
-    turtle.back()
+    turtle.back(len)
     turtle.pendown()
 
 
 # function to draw the triangle
-shape(100, 'green', 3)
+shapes(100, 'green', 3, "left")
+move(250)
+shapes(200, 'blue', 4)
 
 turtle.mainloop()
