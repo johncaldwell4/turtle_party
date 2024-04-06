d686a83d1e5c610e33e99d5cb988af2576c1658c
John Caldwell <john.caldwell@caldwelldevelopment.com>
2024-04-06 06:26:27 -0400

    Created the triangle with a for loop with a function and the triangle is still upside down

--- turtle_party.py
+++ turtle_party.py
@@ -1,10 +1,14 @@
 import turtle
 
-# created the triangle with a for loop and the triangle is still upside down.
+# created the triangle with a for loop with function and the triangle is still upside down.
 
-turtle.color('red')
-for line in range(3):
-    turtle.forward(100)
-    turtle.right(120)
+def shape ():
+    turtle.color('red')
+    for line in range(3):
+        turtle.forward(100)
+        turtle.right(120)
+
+#function to draw the triangle
+shape()
 
 turtle.mainloop()
